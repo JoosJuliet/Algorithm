df2d3121292db323f385fa996e65592e27c00f9a
leejoowon <saven7788@gmail.com>
2017-09-06 13:30:15 +0900

    Update star.cpp

--- star.cpp
+++ star.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a;
+    cin >> a;
+    for(int i = 0; i<a; i++){
+        for(int j = -1 ; j<i; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+    return 0;
+}
