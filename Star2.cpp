12f9618a27cf29d05ce3afeb686b902de5311e03
leejoowon <saven7788@gmail.com>
2017-09-06 13:29:39 +0900

    Update Star2.cpp

--- Star2.cpp
+++ Star2.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for(int i = 0; i<a; i++){
+        for(int k = a ; k > i; k--){
+            if( k != a )
+                cout << " ";
+        }
+        for(int j = -1 ; j<i; j++)
+            cout << "*";
+        
+        cout << endl;
+    }
+    return 0;
+}
