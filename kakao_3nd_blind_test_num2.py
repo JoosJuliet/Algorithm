185f0943d1c516dbdf2abb452c4cd0649f245b34
leejoowon <deff123@hanyang.ac.kr>
2018-08-01 20:47:58 +0900

    Update kakao_3nd_blind_test_num2.py

--- kakao_3nd_blind_test_num2.py
+++ kakao_3nd_blind_test_num2.py
@@ -16,11 +16,17 @@ tmp = ''
 while len(msg) is not 0:
     for i in range(len(msg)):
         i = len(msg) - i
+        # 일부로 숫자가 큰 것 부터 본다. 짧은 것 부터 하면 각 알파벳의  안에 모든 경우의 수가 들어가기 때문이다.
         if msg[0:i] in dictionary :
+            # 무조건 dictionary에 있는 것들만 들어가게 한다.
+            # 어차피 뒤에 있는 것들은 앞에 있는 것들에 의해 dictionary에 무조건 들어가기 때문이다.
             key = msg[0:i]
             next_key = msg[i:i+1] if len(msg) > i+1 else ''
+            # 다음 것이 마지막이면 ''로 보낸다
             ans.append(dictionary[key])
+            # dictionary에 있는 index를 넣는다.
             if next_key is not '' and key+next_key not in dictionary :
+                # 이어진 애가 없으면 dictionary에 넣는다.
                 dictionary[key + next_key] = len(dictionary) + 1
             msg = msg[len(key):]
             break
